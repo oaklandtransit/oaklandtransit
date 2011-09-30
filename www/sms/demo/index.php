19150be12ff6380a2af20ec67c43484b41cdeedc
Krys Freeman <bLaKtivist@gmail.com>
2011-09-29 20:09:52 -0700

    -- cookies introduced to save phone number -- Code submitted by Chris Ward - via email (aka BagelBits)

--- www/sms/demo/index.php
+++ www/sms/demo/index.php
@@ -1,11 +1,21 @@
 <?php
+
 error_reporting(0);
+
+//Cookie code
+//session_start();
+//$numberarray = $_SESSION['numberarray'];
+
+//if(!strlen($numberarray)) {
+//	$numberarray = array();
+//}
 
 include "../../lib/RealtimeData.php";
 
 //$_REQUEST['Body'] = ' #51099 hello ';
 
 $body = trim($_REQUEST['Body']);
+$from = trim($_REQUEST['From']);
 preg_match("/[0-9]{5}/", $body, $matches);
 
 if (!empty( $matches[0] )) {
@@ -31,6 +41,7 @@ function formatMsg($data) {
     }
 
     $msg = substr( trim($msg), 0, -1);
+	$msg .= $from;
 
     return $msg;
 }
